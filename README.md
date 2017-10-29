67f8ebadf8a7f8f26b46f31a51a4d38db3fcd017
Fityan Atqia Rahman <fi.elrahman6@gmail.com>
2017-10-29 17:04:34 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# KlikTasik
---
+++ js/clikZoom.js
@@ -0,0 +1,9 @@
+<script>
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+</script>
